5e3e267b3abf00b2afd5445a91408d0b8e105c3a
Yusuke Nakamura <arugoro01@gmail.com>
2024-04-07 21:21:34 +0900

    feat: essential layout

--- app/(authenticated)/page.tsx
+++ app/(authenticated)/page.tsx
@@ -2,7 +2,7 @@ import {Stack, Typography} from "@mui/material";
 
 export default function Home() {
     return (
-        <Stack height="100lvh" justifyContent="center" alignItems="center">
+        <Stack height="100lvh" width="100%" justifyContent="center" alignItems="center">
             <Typography variant="h3">Welcome to Sports-day Admin</Typography>
         </Stack>
     );
