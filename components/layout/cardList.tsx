02d35df404e0b4fd8191143565db03bad6131e99
Yusuke Nakamura <arugoro01@gmail.com>
2024-04-09 19:52:35 +0900

    fix: previous commit problem2

--- components/layout/cardList.tsx
+++ components/layout/cardList.tsx
@@ -1,8 +1,9 @@
-import {SvgIcon, Grid, Button, Avatar, Card, Stack, Typography, Tooltip} from "@mui/material";
+import {SvgIcon, Grid, Button, Avatar, Chip, Stack, Typography, Tooltip} from "@mui/material";
 import React, {ReactNode} from 'react';
 import {HiClock, HiFlag, HiMapPin, HiTableCells, HiUserGroup} from "react-icons/hi2";
 
-interface CardProps {
+type CardProps = {
+    link?: string;
     sport: string;
     league: string;
     judge: string;
@@ -13,10 +14,10 @@ interface CardProps {
     children?: ReactNode;
 }
 
-const CardList: React.FC<CardProps> = ({location, sport, league, judge, left, right, time, children}) => {
+const CardList: React.FC<CardProps> = ({link, location, sport, league, judge, left, right, time, children}) => {
     return (
         <Grid item xs={12} sm={6} md={4} lg={3}>
-            <Button variant={"contained"} sx={{width: "100%", py: 2, px: 2}}>
+            <Button variant={"contained"} sx={{width: "100%", py: 2, px: 2}} href={link}>
                 <Stack
                     width={"100%"}
                     spacing={1}
@@ -24,7 +25,9 @@ const CardList: React.FC<CardProps> = ({location, sport, league, judge, left, ri
                     alignItems="flex-start"
                 >
                     <Stack spacing={1} direction={"row"} justifyContent={"flex-start"} alignItems="center">
-                        <Avatar sx={{height: "1em", width: "1em"}}>
+                        <Avatar
+                            sx={{height: "1em", width: "1em"}}
+                        >
                         </Avatar>
                         <Typography>{sport}</Typography>
                     </Stack>
