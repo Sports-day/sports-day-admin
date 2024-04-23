4af6c36ea04455296027e5b0b8f41117fdb49275
testusuke <testusuke@gmail.com>
2024-04-23 09:45:38 +0900

    change: cardBackgroundでクリック時の関数を指定できるように変更

--- components/layout/cardBackground.tsx
+++ components/layout/cardBackground.tsx
@@ -1,20 +1,33 @@
 import {Button, Card, Stack, Typography} from "@mui/material";
-import React, { ReactNode } from 'react';
+import React, {ReactNode} from 'react';
 
 type CardProps = {
     title?: string;
     button?: string;
     link?: string;
+    func?: () => void;
     children?: ReactNode;
 };
 
-const CardBackground: React.FC<CardProps> = ({ title, button, link, children }) => {
+const CardBackground: React.FC<CardProps> = ({title, button, link, func, children}) => {
     return (
         <>
-            <Card sx={{py:2, px:2}}>
-                <Stack pb={2}　spacing={1} direction={"row"} justifyContent={"flex-start"} alignItems="center">
+            <Card sx={{py: 2, px: 2}}>
+                <Stack pb={2} spacing={1} direction={"row"} justifyContent={"flex-start"} alignItems="center">
                     {title && <Typography color={"primary.light"}>{title}</Typography>}
-                    {button && <Button variant={"contained"} href={link}>{button}</Button>}
+                    {button &&
+                        <Button
+                            variant={"contained"}
+                            href={link}
+                            onClick={() => {
+                                if (func) {
+                                    func();
+                                }
+                            }}
+                        >
+                            {button}
+                        </Button>
+                    }
                 </Stack>
                 {children}
             </Card>
