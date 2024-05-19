e60fd57332815645038cc26f3ab46f41081808d2
testusuke <testusuke@gmail.com>
2024-05-19 21:16:05 +0900

    feat: 試合一覧を表示するコンポーネントを作成

--- components/match/matchCard.tsx
+++ components/match/matchCard.tsx
@@ -1,22 +1,37 @@
 import {Grid, Button, Avatar, Chip, Stack, Tooltip, Divider} from "@mui/material";
-import React, {ReactNode} from 'react';
 import {HiClock, HiFlag, HiMapPin, HiTableCells, HiUserGroup} from "react-icons/hi2";
+import {Match} from "@/src/models/MatchModel";
+import {sportFactory} from "@/src/models/SportModel";
+import {gameFactory} from "@/src/models/GameModel";
+import {locationFactory} from "@/src/models/LocationModel";
+import {teamFactory} from "@/src/models/TeamModel";
 
-type CardProps = {
-    link?: string;
-    sport: string;
-    league: string;
-    judge: string;
-    left: string;
-    right: string;
-    time: string;
-    location: string;
+type MatchCardProps = {
+    match: Match
 }
 
-const CardList: React.FC<CardProps> = ({link, location, sport, league, judge, left, right, time}) => {
+export default async function MatchCard(props: MatchCardProps) {
+    const sport = await sportFactory().show(props.match.sportId)
+    const game = await gameFactory().show(props.match.gameId)
+    const location = props.match.locationId == null ? undefined : await locationFactory().show(props.match.locationId)
+    const leftTeam = props.match.leftTeamId == null ? undefined : await teamFactory().show(props.match.leftTeamId)
+    const rightTeam = props.match.rightTeamId == null ? undefined : await teamFactory().show(props.match.rightTeamId)
+    const judgeTeam = props.match.judgeTeamId == null ? undefined : await teamFactory().show(props.match.judgeTeamId)
+
+    const date = new Date(props.match.startAt)
+    const formattedDate = `${date.getHours()}時${date.getMinutes() < 10 ? '0' : ''}${date.getMinutes()}分`
+
     return (
         <Grid item xs={12} sm={6} md={4} lg={3}>
-            <Button variant={"contained"} sx={{width: "100%", py: 2, px: 2}} href={link}>
+            <Button
+                variant={"contained"}
+                sx={{
+                    width: "100%",
+                    py: 2,
+                    px: 2
+                }}
+                href={`/sports/${props.match.sportId}/games/${props.match.gameId}/matches/${props.match.id}`}
+            >
                 <Stack
                     width={"100%"}
                     spacing={1}
@@ -25,7 +40,7 @@ const CardList: React.FC<CardProps> = ({link, location, sport, league, judge, le
                 >
 
                     <Stack direction={"row"} spacing={0.1}>
-                        <Tooltip title={`競技 | ${sport}`} placement={"top"} arrow>
+                        <Tooltip title={`競技 | ${sport.name}`} placement={"top"} arrow>
                             <Chip
                                 color="primary"
                                 size={"small"}
@@ -33,7 +48,7 @@ const CardList: React.FC<CardProps> = ({link, location, sport, league, judge, le
                             />
                         </Tooltip>
 
-                        <Tooltip title={`試合会場 | ${location}`} placement={"top"} arrow>
+                        <Tooltip title={`試合会場 | ${location?.name ?? "未登録"}`} placement={"top"} arrow>
                             <Chip
                                 color="primary"
                                 size={"small"}
@@ -41,10 +56,10 @@ const CardList: React.FC<CardProps> = ({link, location, sport, league, judge, le
                             />
                         </Tooltip>
 
-                        <Tooltip title={`試合開始時刻 | ${time}`} placement={"top"} arrow>
+                        <Tooltip title={`試合開始時刻 | ${formattedDate}`} placement={"top"} arrow>
                             <Chip
                                 color="primary"
-                                label={time}
+                                label={formattedDate}
                                 size={"small"}
                                 icon={<HiClock/>}
                             />
@@ -57,11 +72,11 @@ const CardList: React.FC<CardProps> = ({link, location, sport, league, judge, le
                         spacing={1}
                         width={"100%"}
                         sx={{
-                            borderRadius:"10px",
-                            backgroundColor:"#7f8cd6",
-                            p:1.5,
-                            overflow:"auto"
-                    }}
+                            borderRadius: "10px",
+                            backgroundColor: "#7f8cd6",
+                            p: 1.5,
+                            overflow: "auto"
+                        }}
                     >
 
                         <Stack
@@ -72,18 +87,18 @@ const CardList: React.FC<CardProps> = ({link, location, sport, league, judge, le
 
                             <Tooltip title={"リーグ"} placement={"top"} arrow>
                                 <Chip
-                                    sx={{px:0.5}}
+                                    sx={{px: 0.5}}
                                     color="info"
-                                    label={league}
+                                    label={game.name ?? "不明"}
                                     icon={<HiTableCells/>}
                                 />
                             </Tooltip>
 
                             <Tooltip title={"審判のチーム"} placement={"top"} arrow>
                                 <Chip
-                                    sx={{px:0.5}}
+                                    sx={{px: 0.5}}
                                     color="info"
-                                    label={judge}
+                                    label={judgeTeam?.name ?? "未登録"}
                                     icon={<HiFlag/>}
                                 />
                             </Tooltip>
@@ -96,9 +111,9 @@ const CardList: React.FC<CardProps> = ({link, location, sport, league, judge, le
                         >
                             <Tooltip title={"対戦するチーム"} placement={"top"} arrow>
                                 <Chip
-                                    sx={{px:0.5}}
+                                    sx={{px: 0.5}}
                                     color="info"
-                                    label={`${left} vs ${right}`}
+                                    label={`${leftTeam?.name ?? "未登録"} vs ${rightTeam?.name ?? "未登録"}`}
                                     icon={<HiUserGroup/>}
                                 />
                             </Tooltip>
@@ -110,5 +125,3 @@ const CardList: React.FC<CardProps> = ({link, location, sport, league, judge, le
         </Grid>
     );
 };
-
-export default CardList;
