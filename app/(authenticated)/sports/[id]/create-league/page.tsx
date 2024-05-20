470fda37db014f1be8e6d238529d12b9c14d8151
testusuke <testusuke@gmail.com>
2024-05-21 02:40:45 +0900

    change: dndから切り替え

--- app/(authenticated)/sports/[id]/create-league/page.tsx
+++ app/(authenticated)/sports/[id]/create-league/page.tsx
@@ -1,28 +1,30 @@
 import {Breadcrumbs, Link, Stack, Typography} from "@mui/material";
 import CardBackground from "@/components/layout/cardBackground";
-import LeagueDnd from "@/components/league/create/leagueDnd";
 import {sportFactory} from "@/src/models/SportModel";
+import GameForm from "@/components/league/legacy/GameForm";
+import {tagFactory} from "@/src/models/TagModel";
 
 export default async function LeaguePage({params}: { params: { id: string } }) {
     const sportId = parseInt(params.id, 10)
     const sport = await sportFactory().show(sportId)
-    const sportLink = `/sports/${sportId}`
+    const tags = await tagFactory().index()
+
     return (
         <Stack spacing={2} mx={2} my={3}>
-            <Breadcrumbs aria-label="breadcrumb" sx={{pl:2}}>
+            <Breadcrumbs aria-label="breadcrumb" sx={{pl: 2}}>
                 <Link underline="hover" color="inherit" href="/public">
                     管理者のダッシュボード
                 </Link>
                 <Link underline="hover" color="inherit" href={"/sports"}>
                     競技管理
                 </Link>
-                <Link underline="hover" color="inherit" href={sportLink}>
+                <Link underline="hover" color="inherit" href={`/sports/${sportId}`}>
                     {sport.name}
                 </Link>
                 <Typography color="text.primary">リーグを作成・編集</Typography>
             </Breadcrumbs>
-            <CardBackground title={`${sport.name}のリーグ`}>
-                <LeagueDnd sport={sport} sportId={sportId}/>
+            <CardBackground>
+                <GameForm formType={"create"} sportId={sportId} tags={tags}/>
             </CardBackground>
         </Stack>
     );
