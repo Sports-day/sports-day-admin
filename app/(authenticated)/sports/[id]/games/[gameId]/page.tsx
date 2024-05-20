78ac43d00eb28e945bbf9ab32c5c1f8adbccf098
testusuke <testusuke@gmail.com>
2024-05-21 03:15:00 +0900

    feat: リーグエントリー機能の実装

--- app/(authenticated)/sports/[id]/games/[gameId]/page.tsx
+++ app/(authenticated)/sports/[id]/games/[gameId]/page.tsx
@@ -6,8 +6,12 @@ import MatchList from "@/components/match/matchList";
 import LeagueTable from "@/components/league/table/leagueTable";
 import {tagFactory} from "@/src/models/TagModel";
 import GameForm from "@/components/league/legacy/GameForm";
+import {teamTagFactory} from "@/src/models/TeamTagModel";
+import {teamFactory} from "@/src/models/TeamModel";
+import {GameEntryList} from "@/components/league/legacy/GameEntryList";
+import AddGameEntryDialog from "@/components/league/legacy/AddGameEntryDialog";
 
-export default async function GamePage({params}: { params: { gameId:string, id: string } }) {
+export default async function GamePage({params}: { params: { gameId: string, id: string } }) {
     const gameId = parseInt(params.gameId, 10)
     const game = await gameFactory().show(gameId)
     const sportId = parseInt(params.id, 10)
@@ -15,9 +19,17 @@ export default async function GamePage({params}: { params: { gameId:string, id: 
     const matchList = await gameFactory().getGameMatches(gameId)
     const tags = await tagFactory().index()
 
-    return(
+    //  この突貫工事は修正してください。
+    const gameEntryTeams = await gameFactory().getGameEntries(gameId)
+    const teamTags = await teamTagFactory().index()
+    const linkedTeamTag = teamTags.find((teamTag) => teamTag.sportId === sport.id)
+    const teams = await teamFactory().index()
+    const filteredTeams = linkedTeamTag ? teams.filter((team) => team.teamTagId === linkedTeamTag.id) : []
+
+
+    return (
         <Stack spacing={1} mx={2} my={3}>
-            <Breadcrumbs aria-label="breadcrumb" sx={{pl:2}}>
+            <Breadcrumbs aria-label="breadcrumb" sx={{pl: 2}}>
                 <Link underline="hover" color="inherit" href="/">
                     管理者のダッシュボード
                 </Link>
@@ -37,10 +49,25 @@ export default async function GamePage({params}: { params: { gameId:string, id: 
                     tags={tags}
                 />
             </CardBackground>
-            <CardBackground title={`${game.name}のリーグ表`} button={"試合を再生成"} link={`/sports/${sportId}/games/${gameId}/generate-league-matches`}>
-                <LeagueTable game={game} sport={sport} />
+            <CardBackground title={`${game.name}のエントリー`}>
+                <Stack>
+                    <GameEntryList
+                        game={game}
+                        entries={gameEntryTeams}
+                    />
+                    <AddGameEntryDialog
+                        game={game}
+                        teams={filteredTeams}
+                        entries={gameEntryTeams}
+                    />
+                </Stack>
             </CardBackground>
-            <CardBackground title={`${game.name}試合一覧`} button={"一括変更"} link={`/sports/${sportId}/games/${gameId}/automatic-match-editor`}>
+            <CardBackground title={`${game.name}のリーグ表`} button={"試合を再生成"}
+                            link={`/sports/${sportId}/games/${gameId}/generate-league-matches`}>
+                <LeagueTable game={game} sport={sport}/>
+            </CardBackground>
+            <CardBackground title={`${game.name}試合一覧`} button={"一括変更"}
+                            link={`/sports/${sportId}/games/${gameId}/automatic-match-editor`}>
                 <MatchList
                     matches={matchList}
                 />
---
+++ components/league/legacy/GameEntryContent.tsx
@@ -0,0 +1,66 @@
+'use client'
+import {Button, Link, TableCell, TableRow} from "@mui/material";
+import {useState} from "react";
+import {useRouter} from "next/navigation";
+import {Game, gameFactory} from "@/src/models/GameModel";
+import {Team} from "@/src/models/TeamModel";
+import {ConfirmDialog} from "@/components/league/legacy/ConfirmDialog";
+
+export type GameEntryContentProps = {
+    game: Game
+    entryTeam: Team
+}
+
+export function GameEntryContent(props: GameEntryContentProps) {
+    const router = useRouter()
+    const [isDeleteOpen, setIsDeleteOpen] = useState(false)
+
+    const deleteEntry = async () => {
+        await gameFactory().removeGameEntry(
+            props.game.id,
+            props.entryTeam.id
+        )
+
+        //  refresh
+        router.refresh()
+    }
+
+    return (
+        <>
+            <TableRow
+                sx={{'&:last-child td, &:last-child th': {border: 0}}}
+            >
+                <TableCell>
+                    <Link
+                            href={`/admin/teams/${props.entryTeam.id}`}
+                    >
+                        {props.entryTeam.id}
+                    </Link>
+                </TableCell>
+                <TableCell>{props.entryTeam.name}</TableCell>
+                <TableCell>{props.entryTeam.description}</TableCell>
+                <TableCell
+                    sx={{
+                        width: "200px"
+                    }}
+                >
+                    <Button
+                        onClick={() => setIsDeleteOpen(true)}
+                    >
+                        削除
+                    </Button>
+                </TableCell>
+            </TableRow>
+            {/*delete*/}
+            <ConfirmDialog
+                open={isDeleteOpen}
+                onClose={() => setIsDeleteOpen(false)}
+                onConfirm={() => deleteEntry()}
+                confirmText={"削除"}
+                confirmColor={"error"}
+            >
+                <p>{props.entryTeam.name}を削除しますか？</p>
+            </ConfirmDialog>
+        </>
+    )
+}
